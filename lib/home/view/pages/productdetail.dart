4ee1dd1dda89b5ced777e17bfc499634a0b59e41
petereog <ogungbepeter1@gmail.com>
2026-05-06 05:28:18 +1200

    added the product details and cart screen

---
+++ lib/home/view/pages/productdetail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Productdetail extends StatefulWidget {
+  const Productdetail({super.key});
+
+  @override
+  State<Productdetail> createState() => _ProductdetailState();
+}
+
+class _ProductdetailState extends State<Productdetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
